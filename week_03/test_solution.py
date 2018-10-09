4eb1af978f7ef11676dd5add4c8b58b3b27b021e
John Safrit <jsafrit@users.noreply.github.com>
2018-10-09 17:07:08 -0400

    First pass at week_03

---
+++ week_03/test_solution.py
@@ -0,0 +1,33 @@
+import solution
+
+logfilename = 'mini-access-log.txt'
+
+
+def test_read_logs():
+    log_list = solution.logtolist(logfilename)
+    assert len(log_list) == 206
+
+
+def test_got_a_list():
+    log_list = solution.logtolist(logfilename)
+    assert type(log_list) is list
+
+
+def test_all_are_dicts():
+    log_list = solution.logtolist(logfilename)
+    assert all([type(x) is dict
+                for x in log_list])
+
+
+def test_check_keys():
+    log_list = solution.logtolist(logfilename)
+    assert set(log_list[0].keys()) == {'ip_address', 'timestamp', 'request'}
+
+
+def test_check_values():
+    log_list = solution.logtolist(logfilename)
+    first_log_dict = log_list[0]
+
+    assert first_log_dict['ip_address'] == '67.218.116.165'
+    assert first_log_dict['timestamp'] == '30/Jan/2010:00:03:18 +0200'
+    assert first_log_dict['request'] == 'GET /robots.txt HTTP/1.0'
