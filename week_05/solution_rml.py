ef2e3e4107f8c63599965b2e633586d06c286617
John Safrit <jsafrit@users.noreply.github.com>
2018-11-25 18:01:52 -0500

    fixes to make instructor method work

--- week_05/solution_rml.py
+++ week_05/solution_rml.py
@@ -3,12 +3,14 @@
 import requests
 import csv
 
-url = 'https://gist.githubusercontent.com/reuven/77edbb0292901f35019f17edb9794358/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json'
+gist_url = 'https://gist.githubusercontent.com/reuven/77edbb0292901f35019f17edb9794358/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json'
 
-with open('cities.csv', 'w') as outfile:
-    output = csv.writer(outfile, delimiter='\t')
 
-    cities = [(d['city'], d['state'], d['rank'], d['population'])
-              for d in requests.get(url).json()]
+def cities_to_csv(url, filename):
+    with open(filename, 'w', newline='') as outfile:
+        output = csv.writer(outfile, delimiter='\t')
 
-    output.writerows(cities)
+        cities = [(d['city'], d['state'], d['rank'], d['population'])
+                  for d in requests.get(url).json()]
+
+        output.writerows(cities)
